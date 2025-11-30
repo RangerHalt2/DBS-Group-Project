ac5732439f4196a599decd38165aa18cb4395ed8
RangerHalt2 <141537190+RangerHalt2@users.noreply.github.com>
2025-11-29 19:31:07 -0500

    API member lookup by name

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,27 @@ app.post("/api/login", async (req, res) => {
 	}
 });
 
+app.post("/api/member_lookup", async (req, res) => {
+	const { name } = req.body;
+	try {
+		const query = `
+            SELECT * FROM member 
+            WHERE name = $1;
+        `;
+
+		const result = await pool.query(query, [name]);
+
+		if (result.rows.length === 0) {
+			return res.status(404).json({ message: "Member not found" });
+		}
+
+		return res.json({ message: "Member found", member: result.rows[0] });
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ message: "Server error" });
+	}
+});
+
 // Start server
 app.listen(process.env.PORT, () =>
 	console.log(`Server running on port ${process.env.PORT}`)
