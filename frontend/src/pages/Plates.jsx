5455e3b759688d9ab10dd0ec38c931b88cd09356
RangerHalt2 <141537190+RangerHalt2@users.noreply.github.com>
2025-12-01 23:16:07 -0500

    MD:PT24 on plates page and prettified

--- frontend/src/pages/Plates.jsx
+++ frontend/src/pages/Plates.jsx
@@ -1,58 +1,69 @@
 import "../components/styles.css";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 export default function Plates() {
-    const [plates, setPlates] = useState([]);
-    const [sortBy, setSortBy] = useState('quantity');
+	const [plates, setPlates] = useState([]);
+	const [sortBy, setSortBy] = useState("quantity");
 
-    
-    useEffect(() => {
-        const fetchPlates = async () => {
-            try {
-                const response = await fetch('http://localhost:3001/api/buyplates/'+sortBy);
-                
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
+	useEffect(() => {
+		const fetchPlates = async () => {
+			try {
+				const response = await fetch(
+					"http://localhost:3001/api/buyplates/" + sortBy
+				);
 
-                const data = await response.json();
-                setPlates(data);
-            } catch (err) {
-                
-            }
-        };
+				if (!response.ok) {
+					throw new Error(`HTTP error! status: ${response.status}`);
+				}
 
-        fetchPlates();
-    }, [sortBy]);
+				const data = await response.json();
+				setPlates(data);
+			} catch (err) {}
+		};
 
-    return (
-        <div className="home-bg">
-            <div className="profile-form login-card">
-                <button onClick={() => setSortBy('quantity')} disabled={sortBy === 'quantity'} className="sortButton">Sort by Quantity</button>
-                <button onClick={() => setSortBy('price')} disabled={sortBy === 'price'} className="sortButton">Sort By Price</button>
-                {plates.length === 0 ? (
-                    <p>No plates are currently in stock.</p>
-                ) : (
-                    <table className="platetable">
-                        <thead>
-                            <tr>
-                                <th>Description</th>
-                                <th>Quantity</th>
-                                <th>Price</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {plates.map((plate, index) => (
-                                <tr key={index}>
-                                    <td className="platedes">{plate.description}</td>
-                                    <td className="platetd">{plate.quantity}</td>
-                                    <td className="platetd">${plate.price}</td>
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                )}
-            </div>
-        </div>
-    );
-};
+		fetchPlates();
+	}, [sortBy]);
+
+	return (
+		<div className="home-bg md:pt-24">
+			<div className="profile-form login-card">
+				<button
+					onClick={() => setSortBy("quantity")}
+					disabled={sortBy === "quantity"}
+					className="sortButton"
+				>
+					Sort by Quantity
+				</button>
+				<button
+					onClick={() => setSortBy("price")}
+					disabled={sortBy === "price"}
+					className="sortButton"
+				>
+					Sort By Price
+				</button>
+				{plates.length === 0 ? (
+					<p>No plates are currently in stock.</p>
+				) : (
+					<table className="platetable">
+						<thead>
+							<tr>
+								<th>Description</th>
+								<th>Quantity</th>
+								<th>Price</th>
+							</tr>
+						</thead>
+						<tbody>
+							{plates.map((plate, index) => (
+								<tr key={index}>
+									<td className="platedes">{plate.description}</td>
+									<td className="platetd">{plate.quantity}</td>
+									<td className="platetd">${plate.price}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				)}
+			</div>
+		</div>
+	);
+}
