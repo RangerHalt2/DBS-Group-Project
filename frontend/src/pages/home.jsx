eedd76f9859014708b6f943bf21ddd2bdf61ba4d
Snyder111 <43899601+Snyder111@users.noreply.github.com>
2025-12-01 22:33:17 -0500

    Updated homepage mission statement and styling

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -3,12 +3,18 @@ import React from 'react';
 export const Home = () => {
 
 	return <div className="home-bg">
-		<h1 className="login-title">Our Mission</h1>
-		<hr className="hline" />
-		<p className="paragraph">We here at Waste Not Kitchen™ value the community around us. We want to make sure
-			 nobody has to go a single day without a meal. Additionally, we recognize that every days thousands of
-			 meals in restaurants go to waste at the end of the day. That's why we have made a service where restaurants
-			 in the surrounding area are able to sell their extra plates at a discounted price, allowing them to
-			 recoup some losses, but also allowing us to provide a service where people in need are able to eat.</p>
+		<div className="profile-form login-card">
+			<h1 className="title">Our Mission</h1>
+			<hr className="hline" />
+			<p className="paragraph">We here at <i>Waste Not Kitchen™</i> value the community around us. We want to make sure
+				nobody has to go a single day without a meal. Additionally, we recognize that every day thousands of
+				meals in restaurants go to waste at closing time. That's why we have made a service where restaurants
+				in the surrounding area are able to sell their extra plates at a discounted price. This allows them to
+				recoup some losses, but also allows us to provide a service where people in need are able to eat. So 
+				weather you're just here to buy from local restaurant, or you're somone who's willing and able to donate
+				to those who need it, you are supporting a greater cause.</p>
+			<br />
+			<p className='paragraph'>Thank you for your support!</p>
+		</div>
 	</div>;
 };
